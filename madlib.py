29f59b0d92299e6b19f098b9b9486d5256441396
KBaker93 <91436331+KBaker93@users.noreply.github.com>
2021-12-20 21:37:07 -0500

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,10 @@
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+
+madlib = f"Watching Marvel movies is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Spend money and {verb2} like you are {famous_person}!"
+
+print(madlib)
